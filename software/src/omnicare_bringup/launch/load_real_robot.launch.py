29983acb80f41f76f6dcf87bb493af3ce7713c4f
Thiago M <rpj134@gmail.com>
2025-03-07 13:28:18 -0500

    fix zsh init file and fix launch real robot

--- software/src/omnicare_bringup/launch/load_real_robot.launch.py
+++ software/src/omnicare_bringup/launch/load_real_robot.launch.py
@@ -71,7 +71,7 @@ def generate_launch_description():
     )
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-    controller_params_file =  os.path.join(get_package_share_directory('omnicare_control_pkg'), 'config/omnidirectional_controller.yaml') 
+    controller_params_file =  os.path.join(get_package_share_directory('omnidirectional_controllers'), 'config/omnidirectional_controller.yaml') 
     
     controller_manager = Node(
         package="controller_manager",
